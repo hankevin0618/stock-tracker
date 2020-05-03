ab14f03ba3c4052f196ae8581246ddac1fa75060
Kevin Han <kevinhan@Kevinui-MacBookPro.local>
2020-05-04 00:00:17 +1000

    Fetch API working

--- src/components/api.js
+++ src/components/api.js
@@ -1,20 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const API_URL = "http://131.181.190.87:3000";
 
-function GetStocks() {
+function getStocks() {
   const url = `${API_URL}/stocks/symbols`;
 
   return fetch(url)
     .then((res) => res.json())
-    .then((data) => console.log(data)); // 데이터를 가져오긴 함!!
-}
+    .then((data) =>
+      data.map((info) => ({
+        name: info.name,
+        industry: info.industry,
+        symbol: info.symbol,
+      }))
+    );
 
-export default function MyAPI() {
-  return (
-    <div>
-      <h1>This is API</h1>
-      <GetStocks />
-    </div>
-  );
+  // .then((test) => console.log(test.name));
+}
+export function useStocks() {
+  //   const [loading, setLoading] = useState(true);
+  const [stocks, setStocks] = useState([]);
+  //   const [error, setError] = useState(null);
+
+  useEffect(() => {
+    getStocks().then((data) =>
+      data.map((info) => {
+        stocks.push(info);
+      })
+    );
+  });
+
+  return { stocks }; // 애네를 newsheadlines가 아니라 stocktracker에 맞게 바꿔줘야함
 }
--- src/pages/Stocks.js
+++ src/pages/Stocks.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import API from "../components/api.js";
+import { useStocks } from "../components/api.js";
+
+function SayIt(props) {
+  return <h1>{props.title}</h1>;
+}
 
 export default function Stocks() {
+  const { search, setSearch } = useState("");
+  const { stocks } = useStocks([]);
+
   return (
     <div>
       <h1>Stocks</h1>
-      <API />
+      {/* {console.log(stocks)} */}
+      {/*여기다가 스톡을 불러와서 이제 그려넣어야되는데 그게 안됨. 콘솔로그도 안됨 근데 여기다 agGrid쓸생각인데 그렇게 한번해보자*/}
     </div>
   );
 }
